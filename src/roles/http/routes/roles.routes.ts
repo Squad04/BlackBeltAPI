abaad00dee31c28a0ebb9b1e041386aee9c71ed3
Santiago <victor.lacsilva0102@gmail.com>
2024-05-08 16:55:39 -0300

    refactor: separando a responsabilidade de rotas e repository

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,24 +1,14 @@
-import { Role } from "@roles/entities/Role";
 import { Router } from "express";
 import { Request, Response } from "express";
-
-// Provisório antes de adicinar o banco
-
-const roles: Role[] = [];
+import { RolesRepository } from "@roles/repositories/RolesRepository";
 
 const rolesRouter = Router();
+const rolesRepository = new RolesRepository();
 
 rolesRouter.post("/", (req: Request, res: Response) => {
   const { name } = req.body;
 
-  const role = new Role();
-
-  Object.assign(role, {
-    name,
-    created_at: new Date(),
-  });
-
-  roles.push(role);
+  const role = rolesRepository.create({ name });
 
   return res.status(201).json(role);
 });
