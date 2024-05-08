f7df33bad020e55bfaca24050c8fc114e177ef58
Santiago <victor.lacsilva0102@gmail.com>
2024-05-08 16:41:47 -0300

    feat: criando a entidade para as Roles

---
+++ src/roles/roles/entities/Role.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from "uuid";
+
+export class Role {
+  id?: string;
+  name: string;
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+
+    if (!this.created_at) {
+      this.created_at = new Date();
+    }
+  }
+}
