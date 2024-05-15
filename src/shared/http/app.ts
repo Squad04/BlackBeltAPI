53f81d0a3c1bb840b82a44d288d96aba4f912574
Santiago <victor.lacsilva0102@gmail.com>
2024-05-15 15:55:35 -0300

    refactor: segregando as funcionalidades do express para o 'app.ts' e deixando o 'server.ts' mais limpo

---
+++ src/shared/http/app.ts
@@ -0,0 +1,32 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import { routes } from "./routes";
+import { AppError } from "@shared/errors/AppError";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../../swagger.json";
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(routes);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      error: error.message,
+    });
+  }
+
+  console.log(error);
+
+  return res.status(500).json({
+    status: "error",
+    error: "Internal server error",
+  });
+});
+
+export { app };
