3a38f7c9502746c92be82706af1371b804a57c0e
Santiago <victor.lacsilva0102@gmail.com>
2024-05-14 22:53:37 -0300

    feat: implementando o swagger

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,16 +1,17 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { routes } from "./routes";
 import { AppError } from "@shared/errors/AppError";
-
-dotenv.config();
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../../swagger.json";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routes);
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
