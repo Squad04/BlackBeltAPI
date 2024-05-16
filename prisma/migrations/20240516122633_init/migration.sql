335bf33ac501a040ab218d3405f657a509cabbfd
Santiago <victor.lacsilva0102@gmail.com>
2024-05-16 10:06:01 -0300

    feat: fazendo migração da tabela de Role e adicionando o prisma-client ao projeto

---
+++ prisma/migrations/20240516122633_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Role` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `name` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
