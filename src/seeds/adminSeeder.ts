b6d9516087972f0773291643ab5438beb0a8959c
Santiago <victor.lacsilva0102@gmail.com>
2024-05-19 13:51:00 -0300

    feat: implementando seeder de administrador para o banco

---
+++ src/seeds/adminSeeder.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function seedAdmin() {
+  const adminEmail = "admin@email.com";
+  const adminPassword = await hash("password943", 10);
+
+  const adminRole = await prisma.role.upsert({
+    where: {
+      name: "admin",
+    },
+    update: {},
+    create: {
+      name: "admin",
+    },
+  });
+
+  await prisma.user.upsert({
+    where: {
+      email: adminEmail,
+    },
+    update: {},
+    create: {
+      name: "Admin",
+      email: adminEmail,
+      password: adminPassword,
+      roleId: adminRole.id,
+      isAdmin: true,
+    },
+  });
+
+  console.log("Admin created successfully");
+}
+
+seedAdmin()
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
