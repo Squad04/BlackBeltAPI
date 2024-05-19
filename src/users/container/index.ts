eb8ab6053db14b264ed9457c5b2b03d603ac1c25
Santiago <victor.lacsilva0102@gmail.com>
2024-05-19 14:11:21 -0300

    feat: criando rota para exibição do perfil do usuário logado

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -3,6 +3,7 @@ import { UsersRepository } from "@users/repositories/UsersRepository";
 import { CreateLoginController } from "@users/useCases/createLogin/CreateLoginController";
 import { CreateUserController } from "@users/useCases/createUser/CreateUserController";
 import { ListUsersController } from "@users/useCases/listUsers/ListUsersController";
+import { ShowProfileController } from "@users/useCases/showProfile/ShowProfileController";
 import { container } from "tsyringe";
 
 container.registerSingleton<IUsersRepository>(
@@ -13,3 +14,4 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton("CreateUserController", CreateUserController);
 container.registerSingleton("ListUsersController", ListUsersController);
 container.registerSingleton("CreateLoginController", CreateLoginController);
+container.registerSingleton("ShowProfileController", ShowProfileController);
--- src/users/http/routes/users.routes.ts
+++ src/users/http/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { isAuthenticated } from "@shared/http/middlewares/isAuthenticated";
 import { CreateLoginController } from "@users/useCases/createLogin/CreateLoginController";
 import { CreateUserController } from "@users/useCases/createUser/CreateUserController";
 import { ListUsersController } from "@users/useCases/listUsers/ListUsersController";
+import { ShowProfileController } from "@users/useCases/showProfile/ShowProfileController";
 import { NextFunction, Request, Response, Router } from "express";
 import { container } from "tsyringe";
 
@@ -9,6 +10,7 @@ const usersRouter = Router();
 const createUserController = container.resolve(CreateUserController);
 const listUsersController = container.resolve(ListUsersController);
 const createLoginController = container.resolve(CreateLoginController);
+const showProfileController = container.resolve(ShowProfileController);
 
 usersRouter.post("/", (req: Request, res: Response, next: NextFunction) => {
   createUserController.handle(req, res, next);
@@ -26,6 +28,16 @@ usersRouter.post(
   "/login",
   (req: Request, res: Response, next: NextFunction) => {
     createLoginController.handle(req, res, next);
+  },
+);
+
+// Profile route
+
+usersRouter.get(
+  "/profile",
+  isAuthenticated,
+  (req: Request, res: Response, next: NextFunction) => {
+    showProfileController.handle(req, res, next);
   },
 );
 
